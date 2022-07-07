9c78b88d05505b2f816f175225ffcf5659ae264a
Sethu <96852496+nameg1708@users.noreply.github.com>
2022-07-07 19:40:10 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -10,20 +10,20 @@ from userbot import *
 from userbot import bot as mafiabot
 
 DELETE_TIMEOUT = 5
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
-mafia_logo = "./H1M4N5HU0P/mafiabot_logo.jpg"
-h1m4n5hu0p = mafiabot.uid
-mafia = f"[{DEFAULTUSER}](tg://user?id={h1m4n5hu0p})"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Invisible User"
+Invisible_logo = "https://te.legra.ph/file/f8140f4312c7bafd1bf9c.jpg"
+Violence_Dont = invisiblebot.uid
+invisible = f"[{DEFAULTUSER}](tg://user?id={h1m4n5hu0p})"
 
-@mafiabot.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
-@mafiabot.on(sudo_cmd(pattern=r"send (?P<shortname>\w+)", allow_sudo=True))
+@Invisiblebot.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
+@Invisiblebot.on(sudo_cmd(pattern=r"send (?P<shortname>\w+)", allow_sudo=True))
 async def send(event):
     if event.fwd_from:
         return
     message_id = event.message.id
     thumb = mafia_logo
     input_str = event.pattern_match.group(1)
-    omk = f"**⍟ Plugin name ≈** `{input_str}`\n**⍟ Uploaded by ≈** {mafia}\n\n⚡ **[ʟᴇɢᴇɴᴅᴀʀʏ ᴀғ ᴍᴀғɪᴀʙᴏᴛ](t.me/MafiaBot_Support)** ⚡"
+    omk = f"**⍟ Plugin name ≈** `{input_str}`\n**⍟ Uploaded by ≈** {Invisible}\n\n⚡ **[ʟᴇɢᴇɴᴅᴀʀʏ ᴀғ INVISIBLEʙᴏᴛ](t.me/Nikhil_Bots)** ⚡"
     the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
         lauda = await event.client.send_file(
@@ -39,8 +39,8 @@ async def send(event):
     else:
         await edit_or_reply(event, "File not found..... Kek")
 
-@mafiabot.on(admin_cmd(pattern="install$", outgoing=True))
-@mafiabot.on(sudo_cmd(pattern="install$", allow_sudo=True))
+@Invisiblebot.on(admin_cmd(pattern="install$", outgoing=True))
+@Invisiblebot.on(sudo_cmd(pattern="install$", allow_sudo=True))
 async def install(event):
     if event.fwd_from:
         return
@@ -71,7 +71,7 @@ async def install(event):
                             a = "__Installing...__"
                             b = 1
                         await event.edit(a)
-                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {mafia}\n\n{string}\n\n        ⚡ **[ʟᴇɢᴇɴᴅᴀʀʏ ᴀғ ᴍᴀғɪᴀʙᴏᴛ](t.me/MafiaBot_Support)** ⚡", link_preview=False)
+                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {mafia}\n\n{string}\n\n        ⚡ **[ʟᴇɢᴇɴᴅᴀʀʏ ᴀғ InvisibleBᴏᴛ](t.me/Nikhil_Bots)** ⚡", link_preview=False)
                 return await event.edit(f"Installed module `{os.path.basename(downloaded_file_name)}`")
             else:
                 os.remove(downloaded_file_name)
@@ -80,22 +80,22 @@ async def install(event):
             await event.edit(f"**Failed to Install** \n`Error`\n{str(e)}")
             return os.remove(downloaded_file_name)
     
-@mafiabot.on(admin_cmd(pattern=r"uninstall (?P<shortname>\w+)", outgoing=True))
-@mafiabot.on(sudo_cmd(pattern=r"uninstall (?P<shortname>\w+)", allow_sudo=True))
+@Invisiblebot.on(admin_cmd(pattern=r"uninstall (?P<shortname>\w+)", outgoing=True))
+@Invisiblebot.on(sudo_cmd(pattern=r"uninstall (?P<shortname>\w+)", allow_sudo=True))
 async def uninstall(h1m4n5hu0p):
     if h1m4n5hu0p.fwd_from:
         return
-    shortname = h1m4n5hu0p.pattern_match["shortname"]
+    shortname = Violence_Dont.pattern_match["shortname"]
     dir_path =f"./userbot/plugins/{shortname}.py"
     try:
         remove_plugin(shortname)
         os.remove(dir_path)
-        await h1m4n5hu0p.edit(f"Uninstalled `{shortname}` successfully")
+        await Violence_Dont.edit(f"Uninstalled `{shortname}` successfully")
     except OSError as e:
-        await h1m4n5hu0p.edit("Error: %s : %s" % (dir_path, e.strerror))
+        await Violence_Dont.edit("Error: %s : %s" % (dir_path, e.strerror))
 
-@mafiabot.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$"))
-@mafiabot.on(sudo_cmd(pattern=r"upload (?P<shortname>\w+)$", allow_sudo=True))
+@invisiblebot.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$"))
+@invisiblebot.on(sudo_cmd(pattern=r"upload (?P<shortname>\w+)$", allow_sudo=True))
 async def unload(event):
     if event.fwd_from:
         return
@@ -111,8 +111,8 @@ async def unload(event):
         )
 
 
-@mafiabot.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$"))
-@mafiabot.on(sudo_cmd(pattern=r"load (?P<shortname>\w+)$", allow_sudo=True))
+@invisiblebot.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$"))
+@invisiblebot.on(sudo_cmd(pattern=r"load (?P<shortname>\w+)$", allow_sudo=True))
 async def load(event):
     if event.fwd_from:
         return
@@ -140,5 +140,5 @@ CmdHelp("core").add_command(
 ).add_command(
   "send", "<file name>", "Sends the given file from your userbot server, if any.", "send alive"
 ).add_command(
-  "cmds", None, "Gives out the list of modules in mafiabot."
+  "cmds", None, "Gives out the list of modules in invisiblebot."
 ).add()
